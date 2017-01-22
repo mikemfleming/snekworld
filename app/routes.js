bfa32d93cbf3e9ddc5bf8d531e4328dd5b92930b
Michael Fleming <flemingatx@gmail.com>
2017-01-22 15:15:09 -0600

    correcting git submodule

--- app/routes.js
+++ app/routes.js
@@ -24,7 +24,7 @@ module.exports = function (app, passport) {
   // process the login form
   app.post('/login', passport.authenticate('local-login', {
     successRedirect : '/', // redirect to the secure profile section
-    failureRedirect : '/', // redirect back to the signup page if there is an error
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
   // =====================================
@@ -39,7 +39,7 @@ module.exports = function (app, passport) {
   // process the signup form
   app.post('/signup', passport.authenticate('local-signup', {
     successRedirect : '/', // redirect to the secure profile section
-    failureRedirect : '/', // redirect back to the signup page if there is an error
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true, // allow flash messages
   }));
 
