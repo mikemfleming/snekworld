9ec4e7b3af939e07b1c3c41728b63071fbe3d497
Michael Fleming <flemingatx@gmail.com>
2017-01-22 15:35:45 -0600

    first commit

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: 'mongodb://kingdude:Octavia8@jello.modulusmongo.net:27017/Idum9ygo', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  url: `mongodb://${process.env.userSecret}:${process.env.passSecret}@jello.modulusmongo.net:27017/${process.env.idSecret}`,
 };
